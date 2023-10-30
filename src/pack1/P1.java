dea9838e7e874ad8cbec223fed59dbe6bda3b4e7
Renu Garg <renugarg581@gmail.com>
2023-10-30 17:59:26 +0000

    2nd commit by renu

--- src/pack1/P1.java
+++ src/pack1/P1.java
@@ -3,12 +3,13 @@ package pack1;
 public class P1 {
 
 	public static void main(String[] args) {
-		String name = "Amit Vaghela";
-		String surname = name.substring(name.indexOf(' '));
-		surname = surname.trim();
-		System.out.println(surname);
-		String firstname = name.substring(0, name.indexOf(' '));
-		System.out.println(firstname);
+		
+//		String name = "Amit Vaghela";
+//		String surname = name.substring(name.indexOf(' '));
+//		surname = surname.trim();
+//		System.out.println(surname);
+//		String firstname = name.substring(0, name.indexOf(' '));
+//		System.out.println(firstname);
 	}
 
 }

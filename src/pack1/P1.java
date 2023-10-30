b1511284a474232dc99d4f78be63539f9c24a432
RAFATH <rrafathmurtujahokrani@gmail.com>
2023-10-30 18:45:19 +0000

    committed by rafath

--- src/pack1/P1.java
+++ src/pack1/P1.java
@@ -4,8 +4,8 @@ public class P1 {
 
 	public static void main(String[] args) {
 		
-//		String name = "Amit Vaghela";
-//		String surname = name.substring(name.indexOf(' '));
+		String name = "Amit Vaghela";
+		String surname = name.substring(name.indexOf(' '));
 //		surname = surname.trim();
 //		System.out.println(surname);
 //		String firstname = name.substring(0, name.indexOf(' '));
